150094d7d335149618289847ec9bc04025b22e4f
Chad Jessup <chad.jessup@gmail.com>
2021-07-27 16:32:29 -0700

    laying down some foundational files

---
+++ SharpDescent2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+var configurationBuilder = new ConfigurationBuilder()
+    .AddJsonFile($"SharpDescent2.json", optional: true)
+    .AddJsonFile($"SharpDescent2.{Environment.MachineName}.json", optional: true)
+    .AddCommandLine(args);
+
+// pass configuration to GamePlatformBuilder...
+IGamePlatformBuilder platformBuilder = new GamePlatformBuilder(configurationBuilder);
