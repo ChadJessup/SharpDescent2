73830f8e4b15f4a44f45eaa2fb77783b434a9113
Chad Jessup <chad.jessup@gmail.com>
2021-08-22 22:04:44 -0700

    stubbing in blank systems

---
+++ SharpDescent2.Core/Systems/SwitchSystem.cs
@@ -0,0 +1,17 @@
+﻿using Sharp.Platform.Interfaces;
+
+namespace SharpDescent2.Core.Systems;
+
+public class SwitchSystem : IGamePlatformManager
+{
+    public bool IsInitialized { get; }
+
+    public ValueTask<bool> Initialize()
+    {
+        return ValueTask.FromResult(true);
+    }
+
+    public void Dispose()
+    {
+    }
+}
---
+++ SharpDescent2.Core/Systems/UISystem.cs
@@ -0,0 +1,18 @@
+﻿using Sharp.Platform.Interfaces;
+
+namespace SharpDescent2.Core.Systems;
+
+public class UISystem : IGamePlatformManager
+{
+    public bool IsInitialized { get; }
+
+    public ValueTask<bool> Initialize()
+    {
+        return ValueTask.FromResult(true);
+    }
+
+    public void Dispose()
+    {
+    }
+}
+
